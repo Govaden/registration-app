c40c046481fcae0f03c78d3e821b9dc26eaa97a7
Govaden <govaden@gmail.com>
2024-03-22 12:41:39 +0200

    added BindingView, filled activity_main.xml and activity_preview.xml layout

--- app/src/main/java/com/govaden/registrationapp/MainActivity.kt
+++ app/src/main/java/com/govaden/registrationapp/MainActivity.kt
@@ -2,10 +2,22 @@ package com.govaden.registrationapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.govaden.registrationapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
     }
+}
+
+private fun onPreviewClicked() {
+
+
 }
